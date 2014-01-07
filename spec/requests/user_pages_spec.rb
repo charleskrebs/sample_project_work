776436cd8410d8a027227ddd85f0c25c025f15ec
Charles Krebs <charleskrebs@gmail.com>
2014-01-06 16:32:59 -0800

    Finished the sign-up page.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-describe "User Pages" do
+describe 'User Pages' do
   subject { page }
 
-  describe "signup page" do
+  describe 'signup page' do
     before { visit signup_path }
     it { should have_content('Sign up')}
     it { should have_title(full_title('Sign Up')) }
   end
 
-  describe "profile page" do
+  describe 'profile page' do
     let (:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
@@ -17,26 +17,26 @@ describe "User Pages" do
     it { should have_title(user.name) }
   end
 
-  describe "signup page" do
+  describe 'signup page' do
 
     before { visit signup_path }
 
-    let(:submit) { "Create my account" }
+    let(:submit) { 'Create my account' }
 
-    describe "with invalid information" do
-      it "should not create a user" do
+    describe 'with invalid information' do
+      it 'should not create a user' do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
 
-    describe "with valid information" do
+    describe 'with valid information' do
       before do
-        fill_in "Name",     with: "Charles Krebs"
-        fill_in "Email",    with: "a@b.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in 'Name',     with: 'Charles Krebs'
+        fill_in 'Email',    with: 'a@b.com'
+        fill_in 'Password', with: 'foobar'
+        fill_in 'Confirmation', with: 'foobar'
       end
-      it "should create a user" do
+      it 'should create a user' do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
