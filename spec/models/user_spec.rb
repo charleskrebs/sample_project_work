7d9297bfeb08147ed228fa66a5b0dad243308b67
Charles Krebs <charleskrebs@gmail.com>
2014-01-12 19:23:57 -0800

    Added sign-in and sign-out behavior

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
 
@@ -95,6 +96,11 @@ describe User do
     before { @user.password = @user.password_confirmation = 'a' * 5 }
     #noinspection RubyResolve
     it { should be_invalid }
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
 end
